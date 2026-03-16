24548636ceb6a363371215ce9ecaf108ea74535f
ChiragPaul <chiragpaul6474@gmail.com>
2026-03-16 14:15:32 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -3,7 +3,94 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        // Create room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Static availability variables
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        System.out.println("Hotel Room Types and Availability");
+        System.out.println("----------------------------------");
+
+        System.out.println("\nSingle Room");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailability);
+
+        System.out.println("\nDouble Room");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailability);
+
+        System.out.println("\nSuite Room");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailability);
     }
 }
+
+
+
+
+
+abstract class Room {
+
+    /** Number of beds available in the room. */
+    protected int numberOfBeds;
+
+    /** Total size of the room in square feet. */
+    protected int squareFeet;
+
+    /** Price charged per night for this room type. */
+    protected double pricePerNight;
+
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    /** Displays room details */
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq ft");
+        System.out.println("Price per night: $" + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+
+    /**
+     * Initializes a SingleRoom with
+     * predefined attributes.
+     */
+    public SingleRoom() {
+        super(1, 250, 150.0);
+    }
+}
+
+
+ class DoubleRoom extends Room {
+
+    /**
+     * Initializes a DoubleRoom with
+     * predefined attributes.
+     */
+    public DoubleRoom() {
+        super(2, 400, 250.0);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    /**
+     * Initializes a SuiteRoom with
+     * predefined attributes.
+     */
+    public SuiteRoom() {
+        super(3, 750, 599.0);
+    }
+}
+
